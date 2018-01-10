031a1ec34023fda790575e7007eebbeb7147321d
TSHEGOFATSO <mojelatshego@gmail.com>
2018-01-10 16:41:00 +0200

    ds
    ds

--- Envelope_Internal/Envelope_Internal/navPage.xaml.cs
+++ Envelope_Internal/Envelope_Internal/navPage.xaml.cs
@@ -16,5 +16,35 @@ namespace Envelope_Internal
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new navPage());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new navPage());
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new navPage());
+        }
+
+        private async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new navPage());
+        }
+
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new navPage());
+        }
+
+        private async void Button_Clicked_5(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new navPage());
+        }
+    }
 }
