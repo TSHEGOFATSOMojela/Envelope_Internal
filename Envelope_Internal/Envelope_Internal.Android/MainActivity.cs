90cd3d071a13c1d01746607a7d1696c5ab381e50
Lerato-Raloobo <eraloobo@gmail.com>
2018-01-18 12:48:38 +0200

    frehyjt7ik

--- Envelope_Internal/Envelope_Internal.Android/MainActivity.cs
+++ Envelope_Internal/Envelope_Internal.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Envelope_Internal.Droid
 {
-    [Activity(Label = "Envelope_Internal", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Envelope_Internal", Icon = "@drawable/Ekurhulen", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
