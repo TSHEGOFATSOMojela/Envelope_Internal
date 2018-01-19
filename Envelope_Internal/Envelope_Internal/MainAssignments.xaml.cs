a3c083962a658255e296bb11ea9d3c298ef821dc
the1Spartacus <thulanianesh@gmail.com>
2018-01-19 14:35:04 +0200

    adding pages
    adding pages

---
+++ Envelope_Internal/Envelope_Internal/MainAssignments.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Envelope_Internal
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainAssignments : ContentPage
+	{
+		public MainAssignments ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
