4ea8f5675cffe0f62c08e59557b998501a37e5ff
MthunziDuze <mthunziduze@gmail.com>
2018-02-08 11:15:39 +0200

    SQL lite update

--- Envelope_Internal/Envelope_Internal/Indigent/IFileHelper.cs
+++ Envelope_Internal/Envelope_Internal/Indigent/IFileHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Envelope_Internal.Indigent.Models;
-using Envelope_Internal.Indigent.Services;
-
-namespace Envelope_Internal.Indigent.Models
+﻿namespace Envelope_Internal.Indigent.Models
 {
     public interface IFileHelper
     {
