b6fd7e6d80398dd5fbd00a686734a595d4e6e80b
TSHEGOFATSO <mojelatshego@gmail.com>
2018-02-20 14:33:22 +0200

    sdcsd
    sdfs

--- Envelope_Internal/Envelope_Internal/Indigent/Assignment/AccptedAssignmentsList.xaml.cs
+++ Envelope_Internal/Envelope_Internal/Indigent/Assignment/AccptedAssignmentsList.xaml.cs
@@ -41,7 +41,7 @@ namespace Envelope_Internal.Indigent.Assignment
             var myList = await App.Database.GetItemsAsync();
             ArrayList accepted1 = new ArrayList();
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 2; i++)
             {
                 //accepted1.Add(myList[i].applicantDetails);
 
