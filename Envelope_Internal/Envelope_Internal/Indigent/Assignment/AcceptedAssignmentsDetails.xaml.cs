f8d7096db03521060f45d42d6462f06f21dc9b17
TSHEGOFATSO <mojelatshego@gmail.com>
2018-02-21 09:42:02 +0200

    csud
    sshdhcidsh

--- Envelope_Internal/Envelope_Internal/Indigent/Assignment/AcceptedAssignmentsDetails.xaml.cs
+++ Envelope_Internal/Envelope_Internal/Indigent/Assignment/AcceptedAssignmentsDetails.xaml.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Envelope_Internal.Indigent.Accepted;
     using Envelope_Internal.Indigent.ViewModels;
     using Envelope_Internal.Indigent.Models;
     using System.Net.Http;
@@ -31,25 +32,25 @@
             //Living Condition Button
             private async void Living_Conditions(object sender, EventArgs e)
             {
-                await Navigation.PushAsync(new IndigentLoginPage());
+                await Navigation.PushAsync(new LivingConditions());
             }
 
             //HouseHold memebers Button
             private async void HouseHold_Members(object sender, EventArgs e)
             {
-                await Navigation.PushAsync(new IndigentLoginPage());
+                await Navigation.PushAsync(new HouseHoldDetails());
             }
 
             //Signatures Button
             private async void Signatures(object sender, EventArgs e)
             {
-                await Navigation.PushAsync(new IndigentLoginPage());
+                await Navigation.PushAsync(new Signatures());
             }
 
             //Documents Button
             private async void Documents(object sender, EventArgs e)
             {
-                await Navigation.PushAsync(new IndigentLoginPage());
+                await Navigation.PushAsync(new Documents());
             }
         }
     }
