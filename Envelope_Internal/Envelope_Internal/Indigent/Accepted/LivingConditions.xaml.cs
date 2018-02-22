871aa35819f3a459a35c439b15814fdf3c33c103
TSHEGOFATSO <mojelatshego@gmail.com>
2018-02-22 10:26:42 +0200

    sdfvs
    dgsdg

--- Envelope_Internal/Envelope_Internal/Indigent/Accepted/LivingConditions.xaml.cs
+++ Envelope_Internal/Envelope_Internal/Indigent/Accepted/LivingConditions.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Envelope_Internal.Indigent.Accepted;
+using Envelope_Internal.Indigent.ViewModels;
+using Envelope_Internal.Indigent.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
+using SQLite;
 using Xamarin.Forms.Xaml;
 
 namespace Envelope_Internal.Indigent.Accepted
@@ -12,9 +17,11 @@ namespace Envelope_Internal.Indigent.Accepted
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LivingConditions : ContentPage
 	{
-		public LivingConditions ()
+         Indigents indigentDetails;
+        public LivingConditions(Indigents indigentDetails)
 		{
 			InitializeComponent ();
-		}
+            BindingContext = indigentDetails;
+        }
 	}
 }
